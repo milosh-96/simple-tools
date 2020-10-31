bfcb706a44a846457b5a92de7bd40c1477649485
milosh-96 <milosjovanovic042@gmail.com>
2020-10-31 05:48:58 +0100

    test resize

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Respnse;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+});
+
+Route::prefix('/image')->group(function() {
+    Route::get('/',function() {
+        try {
+            $imageBlob = file_get_contents(request()->url);
+            $source = new \Imagick();
+            $source->readImageBlob($imageBlob);
+            $source->resizeImage(256,128,\Imagick::FILTER_BOX, true);
+            $response = Response::make($source, 200)->header("Content-Type","image/jpeg");
+            return $response;
+        }
+        catch(\Exception $e) {
+            return $e->getMessage();
+        }
+        $im = new \Imagick();
+
+    });
 });
