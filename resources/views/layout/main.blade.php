e00988aca72b67bd107ab9ca74ca4564c12e20ae
milosh-96 <milosjovanovic042@gmail.com>
2020-11-17 16:42:53 +0100

    header logo update

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -27,8 +27,8 @@
     <header>
         <div class="row">
             <div class="columns small-12">
-            <div style="font-size: 24px;padding:10px 0">
-                <a href="/">simple<strong>tools</strong></a><span style="color:#003366">:</span><em>online</em>
+            <div style="font-size: 24px;padding:10px 0" id="logo">
+                <a href="/">simple<strong>tools</strong></a><span style="color:#003366">://</span><em>online</em>
             </div>
             </div>
         </div>
