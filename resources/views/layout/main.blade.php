4c5aa2fa1793bb01774ea3c946cde2f72603063f
milosh-96 <milosjovanovic042@gmail.com>
2020-11-17 16:34:23 +0100

    header logo update

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -30,6 +30,7 @@
             <div style="font-size: 24px;padding:10px 0">
                 <a href="/">
                     simple<strong>tools<span style="color:#003366">:</span></strong>
+                    online
                 </a>
             </div>
             </div>
