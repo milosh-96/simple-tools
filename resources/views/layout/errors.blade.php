ab3968962bbe5b1d7f45fa834f3b583327e96de1
milosh-96 <milosjovanovic042@gmail.com>
2020-11-26 04:59:54 +0100

    marged errors.blade from loginTest

--- resources/views/layout/errors.blade.php
+++ resources/views/layout/errors.blade.php
@@ -1,7 +1,23 @@
 @if($viewModel->getErrors())
-        <div class="callout warning">
-            @foreach($viewModel->getErrors() as $error)
-            {{$error}}
-            @endforeach
-        </div>
+<div class="callout warning">
+    {{-- viewmodel errors aka custom feature errors --}}
+    @foreach($viewModel->getErrors() as $error)
+    <div>
+        <strong style="color:red">{{$error}}</strong>
+    </div>
+    @endforeach
+</div>
 @endif
+
+@if($errors->any())
+<div class="callout warning">
+{{--laravel session errors--}}
+    @foreach($errors->all() as $error)
+    <div>
+        <strong style="color:red">{{$error}}</strong>
+    </div>
+    @endforeach
+</div>
+
+@endif
+
