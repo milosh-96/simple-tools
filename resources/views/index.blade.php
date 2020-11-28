49bcb15bae6432f03d622f8edf4202dce5efbbeb
milosh-96 <milosjovanovic042@gmail.com>
2020-11-28 04:20:21 +0100

    index page update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,6 @@
     <p>Simple Tools is a place where you can do various simple things quickly.</p>
 
     <p>Do you want to quickly resize an image, get a random number, make a choice or something else?</p>
-
-    <p>Here you can find a lot of tools. New tools & utilities are under construction and will be available soon!
-    </p>
     <hr>
 
    <!-- <h3>Tips</h3>-->
