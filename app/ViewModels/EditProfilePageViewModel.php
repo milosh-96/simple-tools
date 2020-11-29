97d1ffcd4a90eaebfe5c2c513131d06a3db293b3
milosh-96 <milosjovanovic042@gmail.com>
2020-11-29 06:01:51 +0100

    edit profile

---
+++ app/ViewModels/EditProfilePageViewModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\ViewModels;
+
+class EditProfilePageViewModel extends \App\ViewModels\BasePageViewModel
+{
+     public function __construct()
+    {
+        $this->setTitle("EditProfilePageViewModel");
+    }
+}
