432dcd93223e43642b0f3a38b096e7d0f3aa0bb4
milosh-96 <milosjovanovic042@gmail.com>
2020-11-30 02:43:56 +0100

    updates

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -24,9 +24,7 @@
     <!-- Compressed CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.6.3/css/foundation-float.min.css" integrity="sha512-tuBWy51BW5GZ5BfYddst4eq3d8mcnOk1I5ZHj45P5AUcf9yo5X3KmOJslloaM0ZoSDzS4lcGY91j3L1k5ZVuVg==" crossorigin="anonymous" />
     <link rel="stylesheet" href="/css/app.css" crossorigin="anonymous">
-    @if(request()->route()->getName())
-    <link href="{{url()->current()}}" rel="canonical" />
-    @endif
+    <link href="{{str_replace("https://www.","https://",url()->current())}}" rel="canonical" />
     <!-- Global site tag (gtag.js) - Google Analytics -->
     @yield('additionalHead')
     <script type="application/ld+json">
