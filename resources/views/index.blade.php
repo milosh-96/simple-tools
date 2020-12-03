b572bc71e72469dc1ee20a2911a2d81fbd99ec3d
milosh-96 <milosjovanovic042@gmail.com>
2020-12-03 22:53:02 +0100

    remove questions from home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,12 +3,6 @@
 @include('layout.errors')
 
 <!-- <h3>Tips</h3>-->
-<div class="callout">
-    <h3>Questions</h3>
-<p>Ask poll-like questions. Should I get product X or Y, is X better than Y and similar.</p>
-@include('questions.components.ask')
-@include('questions.components.listview')
-</div>
 
     <h1>Welcome!</h1>
     <p>Simple Tools is a place where you can do various simple things quickly.</p>
