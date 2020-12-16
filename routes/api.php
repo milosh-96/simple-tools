f1c2cbb7795e8c5fddb86a0e508b7ff22b9dc744
milosh-96 <milosjovanovic042@gmail.com>
2020-12-16 04:31:13 +0100

    moved engine endpoints to api prefix

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix'=>'api','middleware' => ['api']], function () {
+
+//routes for raw results (mostly for api endpoints) //
+Route::prefix("engine")->group(function() {
+    Route::prefix("/image")->group(function() {
+        Route::any("/resize",[App\Http\Controllers\Engine\ImageController::class,"resizeImage"])->name("engine.image.resize");
+        Route::any("/crop",[App\Http\Controllers\Engine\ImageController::class,"cropImage"])->name("engine.image.crop");
+        Route::any("/fit-to-canvas",[App\Http\Controllers\Engine\ImageController::class,"fitToCanvas"])->name("engine.image.fitToCanvas");
+        Route::any("/svg-converter",[App\Http\Controllers\Engine\ImageController::class,"svgConverter"])->name("engine.image.svgConverter");
+    });
+
+    Route::prefix('bitcoin')->group(function() {
+        Route::get('get-data',function() {
+            return App\Services\BitcoinConverterService::getData();
+        });
+    });
+
+    Route::prefix("upload")->group(function() {
+        Route::post("/",[UploadController::class,"upload"])->name('upload');
+    });
+});
 });
